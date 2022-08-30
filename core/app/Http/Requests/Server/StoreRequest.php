2d323eace2401f2dc69061db9811d18096b7cedc
amirpasha <g&LVLg4Pjf8N>
2022-08-31 01:51:36 +0430

    Implement StoreRequest

---
+++ core/app/Http/Requests/Server/StoreRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Http\Requests\Server;
+
+
+use App\Models\ServerRam;
+use Illuminate\Validation\Rule;
+
+class StoreRequest extends FormRequ
+{
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'asset_id' => ['required', Rule::unique('servers', 'asset_id')],
+            'price' => ['required', 'numeric', 'min:0.01'],
+            'ram_modules' => ['required', 'array', 'min:1'],
+            'ram_modules.*' => ['required', 'array'],
+            'ram_modules.*.size' => ['required', 'numeric'],
+            'ram_modules.*.type' => ['required', 'string', Rule::in(ServerRam::RAM_TYPES)],
+            'ram_modules.*.amount' => ['required', 'integer', 'min:1'],
+            'brand_id' => ['required', Rule::exists('brands', 'id')]
+        ];
+    }
+}
