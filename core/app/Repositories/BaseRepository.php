1546cde5abe06723be28d45427f77530d25072d2
amirpasha <g&LVLg4Pjf8N>
2022-08-31 01:42:11 +0430

    Add DEFAULT_PAGINATION_LIMIT constant.

---
+++ core/app/Repositories/BaseRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repositories;
+
+
+use App\Contracts\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository implements BaseRepositoryInterface
+{
+    abstract public function getModel();
+
+    public function index()
+    {
+        return $this->getModel()->query();
+    }
+
+    public function create(array $data, bool $runAfterCreate = true)
+    {
+        $model = $this->getModel()->create($data);
+
+        if ($runAfterCreate) {
+            $this->afterCreate($model, $data);
+            $model->refresh();
+        }
+
+        return $model;
+    }
+
+    public function afterCreate(Model $model, array $data)
+    {
+        //
+    }
+
+    public function beforeDelete(Model $model)
+    {
+        return $model;
+    }
+
+    public function delete(Model $model, bool $runBeforeDelete = true)
+    {
+        if ($runBeforeDelete) {
+            $model = $this->beforeDelete($model);
+        }
+
+
+        $model->delete();
+    }
+}
