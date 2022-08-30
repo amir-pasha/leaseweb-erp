5bebb26ab5a56a91f08fa31b095153c983d22ea3
amirpasha <g&LVLg4Pjf8N>
2022-08-31 01:41:04 +0430

    Add migrations

---
+++ core/database/migrations/2022_08_26_101444_create_servers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('asset_id')->unique()->index();
+            $table->string('name')->index();
+            $table->unsignedFloat('price');
+            $table->unsignedInteger('brand_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+};
